f8ff87ddffa9638163971b2acf6b05748fd6c23d
Pier Paolo Ramon <ramonpierre@gmail.com>
2015-03-02 21:15:52 +0100

    Refactored everything into two big funs

--- src/index.js
+++ src/index.js
@@ -7,33 +7,33 @@ export const explode = (list = []) =>
       // …and give back the initial result
       results :
       // We explode every result built so far…
-      results.reduce((memo, result) =>
-        // …by ‘multiplying it’ with every fork we encounter
-        [ ...memo, ...forks.map(fork =>
-          // concat’ing the result with every new possible piece
-          [ ...result, fork ]) ], []), [[]]);
+      results.reduce((memo, result) => [
+          // …in a single array…
+          ...memo,
+          // …by ‘multiplying it’ with every fork we encounter
+          ...forks.map(fork =>
+            // concat’ing the result with every new possible piece
+            [ ...result, fork ])
+        ], []), [[]]);
 
-export const explodeTree = (tree, cfg) =>
+export const explodeTree = (tree, { fork, extract, compose }) =>
   // Fork this tree node into its combinatories
-  cfg.fork(tree)
-    // Then transform every possible path into a single array…
-    .reduce((memo, node) =>
-      // …which is the product between it and its combinatories
-      [ ...memo, ...explodeTreeNode(node, cfg) ], []);
-
-const explodeTreeNode = (node, cfg) => {
-    const list =
-      // Get the children...
-      cfg.extract(node)
-        // …and treat each as a tree…
-        .map(child =>
-          // exploding it (fork = Node => [ Node ])
-          explodeTree(child, cfg));
-
-    // Treat the result as a list of combinatories by exploding it…
-    return explode(list)
+  fork(tree)
+    // Then transform every possible path into…
+    .reduce((memo, node) => [
+      // …a single array…
+      ...memo,
+      // …which is the product between itself and children’s combinatories.
+      ...explode(
+        // So we get the children...
+        extract(node)
+          // …and treat each as a tree…
+          .map(child =>
+            // exploding it (fork = Node => [ Node ])
+            explodeTree(child, { fork, extract, compose }))
+      )
       // …and recompose the result as a node with children,
       .map(children =>
         // and it should return a **new node** every time
-        cfg.compose(node, children));
-};
+        compose(node, children))
+    ], []);
